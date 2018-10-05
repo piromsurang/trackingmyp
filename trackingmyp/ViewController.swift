f68e1435f97f0efbd9020c8f1f36729709154aaa
Piromsurang Rungserichai <piromsurang@Piromsurang.local>
2018-10-05 23:24:12 +0700

    first commit

---
+++ trackingmyp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trackingmyp
+//
+//  Created by Piromsurang Rungserichai on 5/10/2561 BE.
+//  Copyright © 2561 Piromsurang Rungserichai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ trackingmypTests/trackingmypTests.swift
@@ -0,0 +1,34 @@
+//
+//  trackingmypTests.swift
+//  trackingmypTests
+//
+//  Created by Piromsurang Rungserichai on 5/10/2561 BE.
+//  Copyright © 2561 Piromsurang Rungserichai. All rights reserved.
+//
+
+import XCTest
+@testable import trackingmyp
+
+class trackingmypTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
