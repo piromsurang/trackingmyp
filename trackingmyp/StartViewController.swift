a201019dd1a1ff5ecc9b80da01e3ed220141357a
Piromsurang Rungserichai <piromsurang@Piromsurang.local>
2018-10-06 07:19:00 +0700

    temporary set up

---
+++ trackingmyp/StartViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StartViewController.swift
+//  trackingmyp
+//
+//  Created by Piromsurang Rungserichai on 6/10/2561 BE.
+//  Copyright © 2561 Piromsurang Rungserichai. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
